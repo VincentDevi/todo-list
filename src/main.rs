89eceb1c00e444f30543be5c1f3ca3609805c166
Vincent <vincent.devilers@gmail.com>
2024-04-07 22:13:04 +0200

    first template

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
-use axum::{
-    routing::get,
-    Router,
-};
-
+mod templates;
+use axum::{routing::get, Router};
+use templates::*;
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(hello));
 
     tracing::info!("Server running...");
 
     // run our app with hyper, listening globally on port 3000
-    		let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-
-
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
 }
---
+++ src/templates/hello.rs
@@ -0,0 +1,25 @@
+use askama::Template;
+use axum::{
+    body::Body,
+    response::{IntoResponse, Response},
+};
+use hyper::StatusCode;
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate<'a> {
+    name: &'a str,
+}
+
+impl<'a> IntoResponse for HelloTemplate<'a> {
+    fn into_response(self) -> axum::response::Response {
+        Response::new(Body::new(self.render().unwrap()))
+    }
+}
+
+pub async fn hello() -> impl IntoResponse {
+    let test = HelloTemplate {
+        name: "Robin la pute et la zkehahjgejmerde",
+    };
+    (StatusCode::OK, test)
+}
